267e89e8db89552766713c4d0458d33889f855d5
mlamamra <mlamamra@dawan.fr>
2023-03-08 16:34:24 +0100

    enoncé exo de synthèse: 1ère correction

--- exo_moyenne.py
+++ exo_moyenne.py
@@ -1,3 +1,4 @@
+# %%
 # faire un calcul de moyenne arithmétique
 # 1. saisir d'un coup une série de nombres espacé par "," avec input
 # ex. de saisie: 1,2.6,truc,...
@@ -7,3 +8,24 @@
 # 3. les valeurs numériques sont converties dans la même liste
 # 4. calcul de la moyenne, 
 # uniquement s'il n'y a pas eu erreur de saisie
+
+DELIM = ","
+# values = input(f"veuillez saisir des entiers sépars par {DELIM}: ").split(DELIM)
+values = input(f"veuillez saisir des entiers sépars par {DELIM}: ")
+values = values.split(DELIM)
+issues = []
+for i, val in enumerate(values):
+    # if val.isnumeric() or val[0] == "-" and val[1:].isnumeric():
+    if val.isnumeric() or val.startswith("-") and val[1:].isnumeric():
+        values[i] = int(val)
+    else:
+        issues.append(val)
+
+if not issues and values:
+    moy = sum(values) / len(values)
+    print(f"moyenne de {values}: {round(moy, 2)}")
+else:
+    print(f"{issues}: pas des ints !")
+
+# %%
+# %%
