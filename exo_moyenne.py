cc14a6d49c36a9547b2ddd6b4795d58a500cb34d
mlamamra <mlamamra@dawan.fr>
2023-03-08 15:42:08 +0100

    enoncé exo de synthèse: moyenne

---
+++ exo_moyenne.py
@@ -0,0 +1,9 @@
+# faire un calcul de moyenne arithmétique
+# 1. saisir d'un coup une série de nombres espacé par "," avec input
+# ex. de saisie: 1,2.6,truc,...
+# 2. analyser chaque valeur de la saisie pour déterminer s'il s'agît
+# d'un nombre (commencer par entier, puis entier relatif)
+# HINT: regarder la fonction str.isnumeric()
+# 3. les valeurs numériques sont converties dans la même liste
+# 4. calcul de la moyenne, 
+# uniquement s'il n'y a pas eu erreur de saisie
